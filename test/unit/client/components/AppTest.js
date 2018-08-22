86c1099e039524c4e18d2816f8e6125be520002d
Prabakaran <Prabakaran1993@hotmail.com>
2018-08-22 14:17:06 +0530

    #card-12294777 - Configured moch opts for babel & jsdom

--- test/unit/client/components/AppTest.js
+++ test/unit/client/components/AppTest.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import expect from 'expect'
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
 import { shallow, mount } from 'enzyme'
 import App from '../../../../src/client/components/App'
-const wrapper = shallow(<HelloWorld />)
+let wrapper = shallow(<App />)
 describe('App Component', () => {
   it('renders div', () => {
-    expect(wrapper.find('div').text()).toEqual('Hello World')
+    expect(wrapper.find('div').text()).toContain("Welcome")
   })
-  it('renders p', () => {
-    expect(wrapper.find('p').text()).toEqual('Welcome to my world')
+  it('renders react player div', () => {
+    wrapper = shallow(<App number="1"/>)
+    expect(wrapper.find('div').text()).toContain("Welcome");
   })
 })
