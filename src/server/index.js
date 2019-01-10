1e9c9e9c9429c9ee20443c67d657105d750867c7
paranganat <paranganat@ubuntu.ubuntu-domain>
2019-01-10 15:53:06 +0530

    #Tech - Body Parse

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(express.static('src/dist/'));
 app.get('/', function(request, response) {
