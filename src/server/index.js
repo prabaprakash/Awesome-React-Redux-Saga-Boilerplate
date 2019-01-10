74403709ef805f1d5905402c3bdf2500f86107de
paranganat <paranganat@ubuntu.ubuntu-domain>
2019-01-10 15:57:11 +0530

    Tech - Request Promise

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+var rp = require('request-promise');
 var bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
