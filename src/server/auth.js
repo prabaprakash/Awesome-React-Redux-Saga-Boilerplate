bd3ffd40e9701e2a6c665d57ed4afcf1b3536840
prabaprakash <prabaprakash@outlook.com>
2019-09-03 01:31:55 +0530

    #Configured Google Oauth

--- src/server/auth.js
+++ src/server/auth.js
@@ -2,8 +2,8 @@ var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID: 'd',
-    clientSecret: 'd',
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3200/auth/google/callback"
 },
     function (accessToken, refreshToken, profile, cb) {
