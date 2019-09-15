54875de239cacd90b9e95b002b723f4c926d4ccd
prabaprakash <prabaprakash@outlook.com>
2019-09-15 10:47:33 +0530

    #Tech - Package fix

--- src/server/index.js
+++ src/server/index.js
@@ -6,29 +6,29 @@ const fs = require('fs');
 const rp = require('request-promise');
 const bodyParser = require('body-parser')
 const api = require('./routes/api');
-const passport = require('./auth');
+// const passport = require('./auth');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(require('cookie-parser')());
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(require('cookie-parser')());
+// app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.use(express.static('src/dist/'));
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['email', 'openid'] }));
+// app.get('/auth/google',
+//   passport.authenticate('google', { scope: ['email', 'openid'] }));
 
-app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function (req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
+// app.get('/auth/google/callback',
+//   passport.authenticate('google', { failureRedirect: '/login' }),
+//   function (req, res) {
+//     // Successful authentication, redirect home.
+//     res.redirect('/');
+//   });
 
-app.get('/profile', (req, res) => {
-  res.send({ isAuthenticated: req.isAuthenticated(), user: req.user });
-})
+// app.get('/profile', (req, res) => {
+//   res.send({ isAuthenticated: req.isAuthenticated(), user: req.user });
+// })
 app.use('/api', api);
 
 app.get('*', function (req, res) {
